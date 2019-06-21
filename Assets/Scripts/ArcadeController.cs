0d258552a5876518d88d95b3bb670a42d8b5bd8b
Drew Gingerich <gingerich93@gmail.com>
2019-06-20 19:28:03 -0700

    Add basic scripts for selectable navigation.

---
+++ Assets/Scripts/ArcadeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ArcadeController : MonoBehaviour
+{
+    public SelectableNavigator navigator;
+
+    private void Update() {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (Input.GetButton("Submit"))
+        {
+            navigator.Use();
+        }
+
+        if ( move.x == 1)
+        {
+            navigator.MoveRight();
+        }
+        else if (move.x == -1)
+        {
+            navigator.MoveLeft();
+        }
+        else if(move.y == 1)
+        {
+            navigator.MoveUp();
+        }
+        else if (move.y == -1)
+        {
+            navigator.MoveDown();
+        }
+    }
+}
---
+++ Assets/Scripts/SelectableNavigator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class SelectableNavigator : MonoBehaviour
+{
+    public Selectable selected;
+
+    public void Use()
+    {
+        selected.Use();
+    }
+
+    public void MoveLeft()
+    {
+        ChangeSelection(selected.left);
+    }
+
+    public void MoveRight()
+    {
+        ChangeSelection(selected.right);
+    }
+
+    public void MoveUp()
+    {
+        ChangeSelection(selected.up);
+    }
+
+    public void MoveDown()
+    {
+        ChangeSelection(selected.down);
+    }
+
+    private void ChangeSelection(Selectable next)
+    {
+        if (next != null)
+        {
+            selected.Deselect();
+            selected = selected.left;
+            selected.Select();
+        }
+    }
+}
