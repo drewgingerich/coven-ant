09524a285fbfa8ee94cd63da43f4ec7e83d5db2b
Drew Gingerich <gingerich93@gmail.com>
2019-06-22 18:35:48 -0700

    Add effects framework, driven by scriptable objects.

---
+++ Assets/Scripts/EffectsSystem/TranslateEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Effects/Translation")]
+public class TranslationEffect : AdditiveEffect
+{
+    public Vector2 translation = Vector2.one;
+
+    protected override void AbsoluteAction(FeatureController feature)
+    {
+        feature.SetTranslation(translation);
+    }
+
+    protected override void AdditiveAction(FeatureController feature)
+    {
+        feature.Translate(translation);
+    }
+}
