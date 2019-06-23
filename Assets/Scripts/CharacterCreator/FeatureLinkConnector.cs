a09bb16f23e105364d8b4c8080b45040979c5fc5
Drew Gingerich <gingerich93@gmail.com>
2019-06-22 17:30:33 -0700

    Add scripts to handle character features.

---
+++ Assets/Scripts/CharacterCreator/FeatureLinkConnector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FeatureLinkConnector : MonoBehaviour
+{
+    public FeatureLink link;
+    private void Start()
+    {
+        link.feature = GetComponent<FeatureController>();
+    }
+}
