cdf534ebdb551fc5a1d730c2068872fa679452a2
Drew Gingerich <gingerich93@gmail.com>
2019-06-22 21:49:47 -0700

    Add camera script to grab screenshot of player, and snapshot camera prefab."

---
+++ Assets/Scripts/Portrate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct Portrate 
+{
+    public Texture2D snapshot;
+    public new string name;
+    public string date;
+}
---
+++ Assets/Scripts/SnapshotCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SnapshotCamera : MonoBehaviour
+{
+    public int resWidth = 1920;
+    public int resHeight = 1080;
+
+    [System.NonSerialized]
+    public new Camera camera;
+
+    // https://answers.unity.com/questions/22954/how-to-save-a-picture-take-screenshot-from-a-camer.html
+    public Texture2D TakeSnapshot()
+    {
+        RenderTexture rt = new RenderTexture(camera.pixelWidth, camera.pixelHeight, 24);
+        RenderTexture.active = rt;
+        camera.targetTexture = rt;
+        camera.Render();
+
+        Rect snapshotRect = new Rect(
+            resWidth * 0.25f,
+            resHeight * 0.25f,
+            resWidth * 0.5f, 
+            resHeight * 0.5f
+        );
+
+        Texture2D snapshot = new Texture2D(camera.pixelWidth, camera.pixelHeight, TextureFormat.RGB24, false);
+        snapshot.ReadPixels(snapshotRect, 0, 0);
+
+        camera.targetTexture = null;
+        RenderTexture.active = null; // JC: added to avoid errors
+        Destroy(rt);
+
+        return snapshot;
+    }
+}
