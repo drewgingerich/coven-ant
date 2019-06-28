8447a80281c8ae7b9a8e556b33eebb5a9f5708f3
Shea Hunter <shunter@nerdery.com>
2019-06-27 18:03:17 -0700

    added complete and witch laugh

---
+++ Assets/Scripts/SfxManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+    public List<AudioClip> witchLaughs;
+
+    AudioSource m_Audiosource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Audiosource = GetComponent<AudioSource>();
+    }
+
+    public void PlayWitchLaugh()
+    {
+        m_Audiosource.PlayOneShot(witchLaughs[Random.Range(0, witchLaughs.Count)]);
+    }
+}
