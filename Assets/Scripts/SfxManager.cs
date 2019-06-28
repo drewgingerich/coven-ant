da9cec154e3fbbce89abe657e7ea31b7fe692e2e
Shea Hunter <shunter@nerdery.com>
2019-06-27 18:08:11 -0700

    added other sfx to manager

--- Assets/Scripts/SfxManager.cs
+++ Assets/Scripts/SfxManager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class SfxManager : MonoBehaviour
 {
     public List<AudioClip> witchLaughs;
+    public List<AudioClip> antScreams;
+    public List<AudioClip> antConcerned;
+    public AudioClip antGasp;
+    public AudioClip antNervousLaugh;
+    public AudioClip antSigh;
 
     AudioSource m_Audiosource;
 
@@ -16,6 +21,36 @@ public class SfxManager : MonoBehaviour
 
     public void PlayWitchLaugh()
     {
-        m_Audiosource.PlayOneShot(witchLaughs[Random.Range(0, witchLaughs.Count)]);
+        PlayRandom(witchLaughs);
+    }
+
+    public void PlayAntScream()
+    {
+        PlayRandom(antScreams);
+    }
+
+    public void PlayAntConcerned()
+    {
+        PlayRandom(antConcerned);
+    }
+
+    public void PlayAntGasp()
+    {
+        m_Audiosource.PlayOneShot(antGasp);
+    }
+
+    public void PlayAntNervousLaugh()
+    {
+        m_Audiosource.PlayOneShot(antNervousLaugh);
+    }
+
+    public void PlayAntSigh()
+    {
+        m_Audiosource.PlayOneShot(antSigh);
+    }
+
+    void PlayRandom(List<AudioClip> clips)
+    {
+        m_Audiosource.PlayOneShot(clips[Random.Range(0, clips.Count)]);
     }
 }
