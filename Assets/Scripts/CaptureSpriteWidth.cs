cbcc16c5d7159c2503e9c2273943036b40e0b12e
Drew Gingerich <gingerich93@gmail.com>
2019-06-29 00:40:44 -0700

    Create script to scale camera to always fit background sprite width.

---
+++ Assets/Scripts/CaptureSpriteWidth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CaptureSpriteWidth : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    private Camera _camera;
+    private new Camera camera
+    {
+        get
+        {
+            if (_camera == null)
+            {
+                _camera = GetComponent<Camera>();
+            }
+            return _camera;
+        }
+    }
+
+    const float targetAspectRatio = 16 / 9;
+
+    private void Update()
+    {
+        var currentPosition = transform.position;
+        var targetPosition= spriteRenderer.sprite.bounds.center;
+        targetPosition.z = currentPosition.z;
+        transform.position = targetPosition;
+
+        var spriteScale = spriteRenderer.transform.lossyScale;
+        var spriteHalfWidth = spriteRenderer.sprite.bounds.extents.x;
+        spriteHalfWidth *= spriteScale.x;
+        camera.orthographicSize = spriteHalfWidth / camera.aspect;
+    }
+}
